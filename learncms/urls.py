a5e8c28dbc0ed226cfc9634201d0b5eea60df096
Joe Germuska <joe@germuska.com>
2015-07-28 15:33:17 -0500

    something really basic works

--- learncms/urls.py
+++ learncms/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import LessonView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^lesson/(?P<slug>[a-z\-]+)/$', LessonView.as_view()),
 ]
--- learncms/views.py
+++ learncms/views.py
@@ -1,3 +1,20 @@
+from django.conf import settings
+
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.http import Http404
+
+import os.path
 
 # Create your views here.
+class LessonView(TemplateView):
+    template_name = "lesson.html"
+
+    def get_context_data(self, slug, **kwargs):
+        context = super(LessonView, self).get_context_data(**kwargs)
+        try:
+            path = os.path.join(settings.PROJECT_ROOT,'lessons',"{}.html".format(slug))
+            context['content'] = open(path).read()
+        except FileNotFoundError:
+            raise Http404("No lesson {}".format(slug))
+        return context
