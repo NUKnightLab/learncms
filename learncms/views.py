f29db2043c35d7d9e8bb29c88257a7fce6555525
Joe Germuska <joe@germuska.com>
2015-07-30 15:09:18 -0500

    process content

--- learncms/views.py
+++ learncms/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.http import Http404
 
-from django.template import Template, Context
+from lxml.etree import Comment
+from lxml.html import fromstring, tostring
 
-from .models import Lesson
-
+from .models import Lesson, ZoomingImage
 import os.path
 
 # Create your views here.
@@ -21,7 +21,26 @@ class LessonDetailView(DetailView):
         lesson = self.object
         context['title'] = lesson.title
         context['lesson'] = lesson
-        context['zimages'] = dict((z.slug, z) for z in lesson.zimages.all())
-        t = Template(lesson.content)
-        context['evaluated_content'] = t.render(Context(context))
+        context['evaluated_content'] = self.evaluate_content()
         return context
+
+    def evaluate_content(self):
+        """Convert any convenience markup (such as object references) into the ideal markup
+           for delivering to the page.
+        """
+        content = self.object.content
+        element = fromstring(content)
+        self._resolve_zooming_images(element)
+        return tostring(element,encoding='unicode')
+
+    def _resolve_zooming_images(self, element):
+        for i,zi in enumerate(element.findall('.//zooming-image')):
+            if zi.attrib.has_key('ref'):
+                matches = ZoomingImage.objects.filter(slug=zi.attrib['ref'])
+                if len(matches) == 0:
+                    zi.getparent().append(Comment("Invalid slug ref for zooming-image #{}".format(i)))
+                    zi.getparent().remove(zi)
+                else:
+                    zi.attrib['src'] = matches[0].thumbnail.url
+                    zi.attrib['fullSrc'] = matches[0].image.url
+
