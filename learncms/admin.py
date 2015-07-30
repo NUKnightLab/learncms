5595a6e775c26782284dcae223b43a6c40983779
Joe Germuska <joe@germuska.com>
2015-07-30 15:08:50 -0500

    break relationship between lessons and zooming images

--- learncms/admin.py
+++ learncms/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from learncms.models import Lesson, ZoomingImage
 
-class ZoomingImageInline(admin.TabularInline):
-    model = ZoomingImage.lessons.through
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ('title',)
     prepopulated_fields = {"slug": ("title",)}
-    inlines = [ ZoomingImageInline ]
 
 class ZoomingImageAdmin(admin.ModelAdmin):
-    inlines = [ ZoomingImageInline ]    
-    exclude = ('lesson', )
+    list_display = ('slug',)
 
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(ZoomingImage, ZoomingImageAdmin)
--- learncms/models.py
+++ learncms/models.py
@@ -1,35 +1,33 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 
 class Lesson(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     banner_image = models.ImageField(upload_to='lessons')
     content = models.TextField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('lesson-detail', args=(self.slug,))
 
     def __str__(self):
         return self.title
 
 
 class ZoomingImage(models.Model):
-    lessons = models.ManyToManyField(Lesson, related_name='zimages')
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to='zimages')
     thumbnail = ImageSpecField(source='image',
                                processors=[ResizeToFill(313, 207)],
                                format='JPEG',
                                options={'quality': 60})
+
+    def get_absolute_url(self):
+        return self.image.url
+
+
     def __str__(self):
         return self.slug
-
-
-
-class Block(models.Model):
-    lesson = models.ForeignKey(Lesson)
-    position = models.IntegerField(help_text='Index of this block among all blocks of a lesson')
-    content = models.TextField()
-
-    class Meta:
-        ordering = ['position']
