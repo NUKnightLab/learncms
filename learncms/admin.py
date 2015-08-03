9fad42095a20f076f73e618bd42bac598678386a
Joe Germuska <joe@germuska.com>
2015-08-03 13:27:26 -0500

    add fields for created/updated by and at

--- learncms/admin.py
+++ learncms/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from learncms.models import Lesson, ZoomingImage
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'slug', 'updated_at', 'updated_by')
+    exclude = ('created_at', 'updated_at', 'created_by', 'updated_by')
     prepopulated_fields = {"slug": ("title",)}
+
+    def save_model(self, request, obj, form, change):
+        if obj.created_by is None:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()    
 
 class ZoomingImageAdmin(admin.ModelAdmin):
     list_display = ('slug',)
