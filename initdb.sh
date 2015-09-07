9def5d69cb2273a7466482720ebd2c0ce51974c5
Joe Germuska <joe@germuska.com>
2015-09-07 09:19:52 -0500

    update to work with PG 9.4

--- initdb.sh
+++ initdb.sh
@@ -4,4 +4,4 @@ createdb learncms -E UTF8
 dropuser --if-exists learncms
 psql learncms -c "CREATE USER learncms WITH PASSWORD 'default';"
 psql learncms -c 'GRANT ALL PRIVILEGES ON DATABASE "learncms" to learncms;'
-psql learncms -c 'alter user "learncms" grant createdb;' # necessary for Django tests
+psql learncms -c 'alter user "learncms" createdb;' # necessary for Django tests
