657f7380ed6f62b3de92c8f651a70c896eeb08d8
Joe Germuska <joe@germuska.com>
2015-09-21 12:09:58 -0500

    fix relative path in vulcanized assets

--- build_components.sh
+++ build_components.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 command -v vulcanize >/dev/null 2>&1 || { echo >&2 "'vulcanize' not found. Install it with 'npm install -g vulcanize'"; exit 1; }
 vulcanize --inline-css --inline-scripts components/elements/elements.html > static/webcomponents/webcomponents.html
-cp -r components/elements/learn/styles static/webcomponents/
-cp -r components/elements/learn/images static/webcomponents/
+for directory in `find components/elements/learn -type d -depth 1`;
+do
+  cp -r ${directory} static/webcomponents/learn
+  echo "Copied assets from ${directory} to static/webcomponents/learn"
+done
