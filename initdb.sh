6d8530730eb6e1fe0fc94547321b8179b4ec443b
Joe Germuska <joe@germuska.com>
2015-10-02 12:23:44 -0500

    drop harder

--- initdb.sh
+++ initdb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 dropdb --if-exists learncms
 createdb learncms -E UTF8
+psql learncms -c "drop owned by learncms;"
 dropuser --if-exists learncms
 psql learncms -c "CREATE USER learncms WITH PASSWORD 'default';"
 psql learncms -c 'GRANT ALL PRIVILEGES ON DATABASE "learncms" to learncms;'
